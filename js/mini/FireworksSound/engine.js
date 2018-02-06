fbfcb57734b9fcf1501744d103e2fb8fc17b7e41
pdbq <32277149+pdbq@users.noreply.github.com>
2018-02-06 19:47:30 +0200

    Add files via upload

---
+++ js/mini/FireworksSound/engine.js
@@ -0,0 +1,29 @@
+//global
+var WIDTH;
+var HEIGHT;
+var ctx;
+var canvas;
+
+function createCanvas(WIDTH_, HEIGHT_, COLOR, IMG = false)
+{
+	canvas = document.createElement("canvas");
+	ctx = canvas.getContext("2d");
+	WIDTH = WIDTH_;
+	HEIGHT = HEIGHT_;
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
+	canvas.style.position = 'fixed';
+	canvas.style.left = 0;
+	canvas.style.top = 0;
+	document.body.appendChild(canvas);
+    canvas.style.background = COLOR;
+    if(IMG)
+    {
+    	canvas.style.background = "url(" + IMG + ")";
+    }
+}
+function cls()
+{
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
